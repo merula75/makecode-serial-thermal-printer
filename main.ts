73680471602e860818c1f7a2dc060bc828feb549
merula75 <144024346+merula75@users.noreply.github.com>
2023-12-03 13:50:40 +0100

    initial block test

--- main.ts
+++ main.ts
@@ -1,3 +1,10 @@
-basic.forever(function () {
-	
-})
+/**
+ * Support library for serial thermal printer.
+ */
+//% color=190 weight=100 icon="\uf02f" block="Thermal Printer"
+namespace thermal_printer {
+    //% block"
+    export function print_text(text: string): void {
+        serial.writeString(text);
+    }
+}
