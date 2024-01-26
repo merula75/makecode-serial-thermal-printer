8ed392bb455501783e80bca81b5948929b2010b6
merula75 <144024346+merula75@users.noreply.github.com>
2024-01-26 15:38:41 +0100

    Aktualisiere pxt.json, main.ts

--- main.ts
+++ main.ts
@@ -3,8 +3,22 @@
  */
 //% color=190 weight=100 icon="\uf02f" block="Thermal Printer"
 namespace thermal_printer {
+    let crlfcmd = pins.createBuffer(2);
+    crlfcmd.setUint8(0, 0x0D);
+    crlfcmd.setUint8(1, 0x0A);
+
+    //% block"
+    export function init_printer(tx_pin: SerialPin, rx_pin: SerialPin): void {
+        serial.redirect(tx_pin, rx_pin, BaudRate.BaudRate9600);
+        let initcmd = pins.createBuffer(2);
+        initcmd.setUint8(0, 0x1B);
+        initcmd.setUint8(1, 0x40);
+        serial.writeBuffer(initcmd);
+    }
+    
     //% block"
     export function print_text(text: string): void {
         serial.writeString(text);
+        serial.writeBuffer(crlfcmd);
     }
 }
